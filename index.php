3f8b8422cd9ec23d91f0297f4fbdf28d3570be59
unknown <veysel2529@gmail.com>
2020-08-11 12:10:41 +0300

    ilg gönderme

---
+++ index.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta charset="utf-8">
+<title>PROJE</title>
+
+</head>
+
+<body>
+<form action="sonuç.php" method="post" >
+<table width_"500" border="0" aling="center" cellpadding="0" cellspacing="0">
+<tr>
+<td width="150" height="30">Adınız Soyadınız</td>
+<td width="20" height="30">:</td>
+<td width="330" height="30"><input type="text" name="adisoyadi"></td>
+</tr>
+<tr>
+<td width="150" height="30">Telefon Numaranız</td>
+<td width="20" height="30">:</td>
+<td width="330" height="30"><input type="text" name="telefon"></td>
+</tr>
+<tr>
+<td width="150" height="30">E_Mail Adresiniz</td>
+<td width="20" height="30">:</td>
+<td width="330" height="30"><input type="email" name="emailadresi"></td>
+</tr>
+<tr>
+<td width="150" height="30">Konu</td>
+<td width="20" height="30">:</td>
+<td width="330" height="30"><input type="text" name="konusu"></td>
+</tr>
+<tr>
+<td width="150" height="30">Mesaj</td>
+<td width="20" height="30">:</td>
+<td width="330" height="30"><textarea name="mesaj"></textarea></td>
+</tr>
+<tr>
+<td width="150" height="30">&nbsp;</td>
+<td width="20" height="30">&nbsp;</td>
+<td width="330" height="30"><input type="submit" value="Gönder"></td>
+</tr>
+
+
+
+</table>
+</body>
+</html>
+
+     
+	 
